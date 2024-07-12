af9f87dcc18ba3ca7b60379f360c9d1674ff1da5
Danilo Mendes <danilo.mirandha@gmail.com>
2024-07-12 09:05:49 -0300

    feat: add Header component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { useSaveTodos } from "./hooks/useSaveTodos";
 
+import styles from "./App.module.css";
+import { Header } from "./components/Header";
+
 const App = () => {
   // Monitoring all changes on todos and save in localStorage
   useSaveTodos();
 
-  return <div>App</div>;
+  return (
+    <div className={styles.container}>
+      <header>
+        <Header />
+      </header>
+    </div>
+  );
 };
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { TodoContextProvider } from "./contexts/TodoContext.tsx";
 
-import "./index.css";
+import "./index.module.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
